67e29e0a7829f65c6e210fa18611d327e12e0bb8
riteg21 <santalov445@gmail.com>
2025-10-06 14:17:08 +0400

    refactoring: header

--- src/components/HomePage/header/Header.jsx
+++ src/components/HomePage/header/Header.jsx
@@ -1,7 +1,7 @@
 export const Header = () => {
   return (
     <div className="w-full">
-      <header className="flex items-center justify-between px-4 py-1 sm:px-6 lg:px-40 lg:py-4 bg-white">
+      <header className="flex items-center justify-center px-4 py-1 sm:px-6 lg:px-40 lg:py-4 lg:justify-between bg-white">
         <div>
           <img
             src="/logo.svg"
@@ -10,7 +10,7 @@ export const Header = () => {
           />
         </div>
 
-        <div className=" md:flex  space-x-6 lg:space-x-10 text-gray-500 font-medium">
+        <div className="hidden md:flex space-x-6 lg:space-x-10 text-gray-500 font-medium">
           <a
             href="https://samgtu.ru/bachelors/bachelors-faculty-iait"
             className="duration-200 ease-in hover:text-gray-700 transition-colors"
