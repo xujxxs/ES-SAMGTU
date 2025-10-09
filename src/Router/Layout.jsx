91e6e5081ef2d7ac0061bda7ef216e2f9d938ee3
riteg21 <santalov445@gmail.com>
2025-10-09 14:59:56 +0400

    fixed: footer in layout; adaptive to results

--- src/Router/Layout.jsx
+++ src/Router/Layout.jsx
@@ -8,34 +8,186 @@ export const Layout = () => {
   const isHomePage = useMemo(() => pathname === "/", [pathname]);
 
   return (
-    <div className="w-full" ref={headerRef}>
+    <div className="w-full min-h-screen flex flex-col" ref={headerRef}>
       {!isHomePage && (
-        <header className="flex items-center justify-center px-4 py-1 sm:px-6 lg:px-40 lg:py-4 lg:justify-between bg-white">
-          <Link to={"/"}>
-            <img
-              src="/logo.svg"
-              alt="Логотип"
-              className="w-25 h-25 sm:w-10 sm:h-10 lg:w-auto lg:h-auto"
-            />
-          </Link>
+        <div className="flex flex-col min-h-screen">
+          <header className="flex items-center justify-center px-4 py-1 sm:px-6 lg:px-40 lg:py-4 lg:justify-between bg-white">
+            <Link to={"/"}>
+              <img
+                src="/logo.svg"
+                alt="Логотип"
+                className="w-25 h-25 sm:w-10 sm:h-10 lg:w-auto lg:h-auto"
+              />
+            </Link>
 
-          <div className="hidden md:flex  space-x-6 lg:space-x-10 text-gray-500 font-medium">
-            <a
-              href="https://samgtu.ru/bachelors/bachelors-faculty-iait"
-              className="duration-200 ease-in hover:text-gray-700 transition-colors"
-            >
-              О нас
-            </a>
-            <a
-              href="/"
-              className="duration-200 ease-in hover:text-gray-700 transition-colors"
-            >
-              Как это работает
-            </a>
-          </div>
-        </header>
+            <div className="hidden md:flex space-x-6 lg:space-x-10 text-gray-500 font-medium">
+              <a
+                href="https://samgtu.ru/bachelors/bachelors-faculty-iait"
+                className="duration-200 ease-in hover:text-gray-700 transition-colors"
+              >
+                О нас
+              </a>
+              <a
+                href="/"
+                className="duration-200 ease-in hover:text-gray-700 transition-colors"
+              >
+                Как это работает
+              </a>
+            </div>
+          </header>
+
+          <main className="flex-1">
+            <Outlet />
+          </main>
+
+          <footer className="bg-white mt-auto">
+            <div className="mx-auto w-full max-w-screen-xl">
+              <div className="px-4 py-6 lg:py-8">
+                <div className="flex flex-col md:flex-row md:items-start md:justify-between gap-8 mb-8">
+                  <div className="flex-shrink-0 me-8">
+                    <img
+                      src="/logo.svg"
+                      alt="Логотип СамГТУ"
+                      className="w-30 h-30 md:w-30 md:h-30"
+                    />
+                  </div>
+
+                  <div className="grid grid-cols-2 gap-8 md:grid-cols-4 flex-1">
+                    <div>
+                      <ul className="text-gray-500 font-semibold">
+                        <li className="mb-4">
+                          443100, Самара <br />
+                          Ул. Молодогвардейская, 244, главный корпус
+                        </li>
+                        <li className="mb-4">8 (846) 278-43-11</li>
+                        <li className="mb-4">
+                          <a
+                            href="#"
+                            className="text-blue-600 hover:text-blue-800"
+                          >
+                            rector@samgtu.ru
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                    <div>
+                      <ul className="text-gray-500 font-semibold">
+                        <li className="mb-4">
+                          <h3 className="text-gray-700 font-bold">
+                            Приемная комиссия
+                          </h3>
+                          +7 (800) 302-17-71
+                        </li>
+                        <li className="mb-4">
+                          <h3 className="text-gray-700 font-bold">
+                            Приемная комиссия
+                          </h3>
+                          <h3 className="text-gray-700 font-bold">
+                            Заочное обучение
+                          </h3>
+                          + 7 (846) 279-03-58
+                        </li>
+                      </ul>
+                    </div>
+                    <div>
+                      <ul className="text-gray-500 font-bold">
+                        <li className="mb-4">
+                          <a
+                            href="https://priem.samgtu.ru/"
+                            className="hover:text-blue-800"
+                          >
+                            ПОСТУПАЮЩИМ
+                          </a>
+                        </li>
+                        <li className="mb-4">
+                          <a
+                            href="https://samgtu.ru/students/students"
+                            className="hover:text-blue-800"
+                          >
+                            ОБУЧАЮЩИМСЯ
+                          </a>
+                        </li>
+                        <li className="mb-4">
+                          <a
+                            href="https://samgtu.ru/business/business"
+                            className="hover:text-blue-800"
+                          >
+                            БИЗНЕСУ
+                          </a>
+                        </li>
+                        <li className="mb-4">
+                          <a
+                            href="https://samgtu.ru/science/science"
+                            className="hover:text-blue-800"
+                          >
+                            НАУКА
+                          </a>
+                        </li>
+                        <li className="mb-4">
+                          <a
+                            href="https://samgtu.ru/contacts"
+                            className="hover:text-blue-800"
+                          >
+                            КОНТАКТЫ
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                    <div>
+                      <ul className="text-gray-500 font-semibold">
+                        <li className="mb-4">
+                          <a
+                            href="https://samgtu.ru/bachelors/bachelors-dormitory"
+                            className="hover:text-blue-800"
+                          >
+                            Общежития
+                          </a>
+                        </li>
+                        <li className="mb-4">
+                          <a
+                            href="https://military.samgtu.ru/"
+                            className="hover:text-blue-800"
+                          >
+                            Военнный учебный центр
+                          </a>
+                        </li>
+                        <li className="mb-4">
+                          <a
+                            href="https://samgtu.ru/admission/admission-faq"
+                            className="hover:text-blue-800"
+                          >
+                            Часто задаваемые вопросы
+                          </a>
+                        </li>
+                        <li className="mb-4">
+                          <a
+                            href="https://mail.samgtu.ru/"
+                            className="hover:text-blue-800"
+                          >
+                            Почта
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="pt-6 border-t border-gray-200">
+                  <span className="text-sm text-gray-500 sm:text-center">
+                    © 2014-2025{" "}
+                    <a href="https://samgtu.ru/" className="hover:underline">
+                      СамГТУ
+                    </a>
+                    . Все права защищены.
+                  </span>
+                </div>
+              </div>
+            </div>
+          </footer>
+        </div>
       )}
-      <Outlet />
+
+      {isHomePage && <Outlet />}
     </div>
   );
 };
--- src/components/SurveyPage/Results.jsx
+++ src/components/SurveyPage/Results.jsx
@@ -37,30 +37,43 @@ export const Results = ({ scores }) => {
   console.log(topDirectionItems);
 
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center">
-      <div className="text-center max-w-4xl mx-auto px-4 mt-10">
-        <div className="flex justify-center mb-6">
-          <img src="/congratulations.svg" alt="Happy" />
+    <div className="min-h-screen flex flex-col justify-center items-center py-8 lg:py-0">
+      <div className="text-center max-w-4xl mx-auto px-4 lg:px-6 mt-4 lg:mt-10">
+        <div className="flex justify-center mb-4 lg:mb-6">
+          <img
+            src="/congratulations.svg"
+            alt="Happy"
+            className="w-16 h-16 sm:w-20 sm:h-20 lg:w-24 lg:h-24"
+          />
         </div>
-        <h1 className="text-2xl font-bold text-gray-700 mb-4">Поздравляем!</h1>
-        <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl">
+        <h1 className="text-xl sm:text-2xl lg:text-3xl font-bold text-gray-700 mb-3 lg:mb-4">
+          Поздравляем!
+        </h1>
+
+        <p className="mb-6 lg:mb-8 text-sm sm:text-base lg:text-lg font-normal text-gray-500 md:text-xl px-2 sm:px-0">
           На основе вашего опроса мы определили идеальные направления для
           поступления на ИАИТ. Эти рекомендации учитывают ваши интересы, сильные
           стороны и карьерные цели.
         </p>
       </div>
-      <h2 className=" text-3xl font-bold text-gray-700 mt-20 mb-14">
+
+      <h2 className="text-xl sm:text-2xl lg:text-3xl font-bold text-gray-700 mt-8 lg:mt-20 mb-8 lg:mb-14 px-4 text-center">
         Топ рекомендуемых направлений
       </h2>
-      <div className="flex justify-center flex-wrap gap-6 max-w-6xl mx-auto px-4 mb-19">
+
+      <div className="flex flex-col sm:flex-row justify-center items-center gap-4 lg:gap-6 max-w-6xl mx-auto px-4 lg:px-6 mb-8 lg:mb-19 w-full">
         {topDirectionItems.map((directionItem, index) => (
-          <DirectionItem
+          <div
             key={index}
-            id={index}
-            image={directionItem.image}
-            name={directionItem.name}
-            code={directionItem.number}
-          />
+            className="w-full sm:w-auto sm:flex-1 max-w-sm lg:max-w-none"
+          >
+            <DirectionItem
+              id={index}
+              image={directionItem.image}
+              name={directionItem.name}
+              code={directionItem.number}
+            />
+          </div>
         ))}
       </div>
     </div>
